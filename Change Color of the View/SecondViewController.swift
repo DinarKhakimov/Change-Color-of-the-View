1cc42113ba5dcd9365b2f61cd43dbb4b33583128
Johnny Boshechka <in_order@icloud.com>
2021-12-27 02:03:21 +0500

    Home work with delegate

--- Change Color of the View/SecondViewController.swift
+++ Change Color of the View/SecondViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SecondViewController: UIViewController {
     
     @IBOutlet weak var colorView: UIView! {
         didSet{
@@ -30,15 +30,24 @@ class ViewController: UIViewController {
             valueBlueLabel.textColor = .white
         }
     }
+    @IBOutlet weak var redColorTextField: UITextField!
+    @IBOutlet weak var greenColorTextField: UITextField!
+    @IBOutlet weak var blueColorTextField: UITextField!
+    
     
     @IBOutlet weak var redColorSlider: UISlider!
     @IBOutlet weak var greenColorSlider: UISlider!
     @IBOutlet weak var blueColorSlider: UISlider!
     
+    var delegate: SecondViewControllerDelegate!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
         
+        redColorTextField.delegate = self
+        greenColorTextField.delegate = self
+        blueColorTextField.delegate = self
         
         viewColorChange()
         setValue(for: valueRedLabel, valueGreenLabel, valueBlueLabel)
@@ -48,20 +57,36 @@ class ViewController: UIViewController {
         setupSlider(slider: blueColorSlider, color: .blue)
     }
     
+    @IBAction func donePressedButton(_ sender: UIButton) {
+        viewColorChange()
+        delegate.setBackgroundCololor(for: UIColor(red: CGFloat(redColorSlider.value),
+                                                   green: CGFloat(greenColorSlider.value),
+                                                   blue: CGFloat(blueColorSlider.value),
+                                                   alpha: 1))
+        view.endEditing(true)
+        dismiss(animated: true)
+
+    }
+    
     @IBAction func rgbChangeSlider(_ sender: UISlider) {
         viewColorChange()
         switch sender {
         case redColorSlider:
             setValue(for: valueRedLabel)
+            setValue(for: redColorTextField)
         case greenColorSlider:
             setValue(for: valueGreenLabel)
+            setValue(for: greenColorTextField)
+
         default:
             setValue(for: valueBlueLabel)
+            setValue(for: blueColorTextField)
+
         }
     }
 }
 
-extension ViewController {
+extension SecondViewController {
     
     private func viewColorChange() {
         colorView.backgroundColor = UIColor(red: CGFloat(redColorSlider.value),
@@ -83,11 +108,43 @@ extension ViewController {
                 label.text = string(from: greenColorSlider)
             default:
                 label.text = string(from: blueColorSlider)
+
+            }
+        }
+        }
+        private func setValue(for textFields: UITextField...) {
+            textFields.forEach { textField in
+                switch textField {
+                case redColorTextField:
+                    textField.text = string(from: redColorSlider)
+                case greenColorTextField:
+                    textField.text = string(from: greenColorSlider)
+                default:
+                    textField.text = string(from: blueColorSlider)
+
             }
         }
     }
-    
+
     private func string(from slider: UISlider) -> String {
         String(format: "%.2f", slider.value)
+    }
+}
+
+extension SecondViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        guard let newValue = textField.text else { return }
+        guard let floatValue = Float(newValue) else { return }
+        
+        if textField == redColorTextField {
+            redColorSlider.value = floatValue
+            setValue(for: valueRedLabel)
+        } else if textField == greenColorTextField {
+            greenColorSlider.value = floatValue
+            setValue(for: valueGreenLabel)
+        } else {
+            blueColorSlider.value = floatValue
+            setValue(for: valueBlueLabel)
+        }
     }
 }
